0d6190cf5ae9cb489f1a7ed02c4c169b3b5bc7ad
Zycebx <142185913+Zycebx@users.noreply.github.com>
2024-08-22 19:37:19 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 img="";
+Status="";
+
 function preload()
 {
     img = loadImage("dog_cat.jpg");
@@ -8,6 +10,8 @@ function setup()
 {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function draw()
@@ -18,4 +22,26 @@ function draw()
     noFill();
     stroke("#FF0000")
     rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
 }
